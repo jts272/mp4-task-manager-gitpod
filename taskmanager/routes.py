b07a908996912f3f4d3dfcf0fbde390f9c5ddb0a
jts272 <jts272@outlook.com>
2023-03-31 12:13:23 +0000

    feat: handle form methods with Python

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+# request used for form processing
+# redirect and url_for used on form POST
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 # Generate the database from our package (schema for Postgres).
 from taskmanager.models import Category, Task
@@ -17,4 +19,18 @@ def categories():
 # Supply http methods as we are submitting a form in this view
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        # New instance of the imported Category model
+        # We supply the key the model is expecting ('category_name'),
+        # then populate this var with the input from the form field with
+        # matching name attr.
+        category = Category(category_name=request.form.get("category_name"))
+        # We can now add and commit this form data to the SQLAlchemy
+        # database var of 'db', using the database sessionmaker instance
+        db.session.add(category)
+        db.session.commit()
+        # Redirect to the categories() function using Flask imports
+        return redirect(url_for("categories"))
+
+    # Default 'GET' behaviour (pseudo-else condition):
     return render_template("add_category.html")
